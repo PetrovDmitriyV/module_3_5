3da22447c0613d27ba082770943b9955210c9a2c
PetrovDmitriyV <phanl2@mail.ru>
2024-09-25 15:32:26 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,16 +1,9 @@
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number = str_number.replace('0', '')
     first = int(str_number[0])
     if len(str_number) == 1:
         return first
-    i = 0
-    while i < (len(str_number) - 1):
-        i += 1
-        a = int(str_number[i])
-        if a > 0:
-            first = a
-        else:
-            break
 
     return first * get_multiplied_digits(int(str_number[1:]))
 
